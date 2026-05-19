edff0ded237ccaf762a0c43978d3027c90f7f845
Muma <mumahub1219@gmail.com>
2026-05-19 15:07:16 +0900

    [UI] 도감 UI, 슬롯 UI 구성

---
+++ Assets/Scripts/leeSunHak_Scripts/DictionarySlotUI.cs
@@ -0,0 +1,67 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DictionarySlotUI : MonoBehaviour
+{
+    [Header("슬롯 기본 정보")]
+    [SerializeField] private Image Image_MainIcon;
+    [SerializeField] private Text Text_Name;
+    [SerializeField] private GameObject GObj_Selected;
+    [SerializeField] private UIButton Button_SlotClick;
+
+    private event Action<string> _onclickSlot;
+
+    private string _slotDataId;
+
+    private void OnEnable()
+    {
+        Button_SlotClick.BindOnClickButtonEvent(Onclick_DictionarySlot);
+    }
+
+    private void OnDisable()
+    {
+        _onclickSlot = null;
+    }
+
+    public string GetSlotDataId()
+    {
+        return _slotDataId;
+    }
+
+    public void Onclick_DictionarySlot()
+    {
+        _onclickSlot?.Invoke(_slotDataId);
+    }
+
+    public void InitSlot(string dataId, Action<string> OnclickCallback)
+    {
+        var itemData = GameDataManager.Instance.GetItemData(dataId);
+        if (itemData == null) 
+        {
+            return;
+        }
+
+        Text_Name.text = itemData.Name;
+
+        string iconPath = itemData.IconPath;
+        if (string.IsNullOrEmpty(iconPath) == true)
+        {
+            return;
+        }
+        // 묻지마 사용 < image에 아이콘, sprite리소스 불러와 줄 때
+        GameUtil.LoadAndSetSpriteImage(Image_MainIcon, iconPath).Forget();
+
+        _slotDataId = dataId;
+
+        _onclickSlot += OnclickCallback;
+
+    }
+
+    public void SetSelectedUI(bool isSelect)
+    {
+        GObj_Selected.SetActive(true);
+    }
+}
--- Assets/Scripts/leeSunHak_Scripts/DictionaryUI.cs
+++ Assets/Scripts/leeSunHak_Scripts/DictionaryUI.cs
@@ -1,5 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Triggers;
+
 
 public class DictionaryUI : UIBase
 {
@@ -18,17 +22,94 @@ public class DictionaryUI : UIBase
     [Header("슬롯 리스트 영역")]
     [SerializeField] private Transform Transform_SlotRoot;
 
+    private Dictionary<string, DictionarySlotUI> _slotList = new Dictionary<string, DictionarySlotUI>(); 
 
     private void OnEnable()
     {
+        ReadItemListAndCreateSlot();
+
         Button_CloseBG.BindOnClickButtonEvent(Onclick_CloseDictionary);
         Button_Close.BindOnClickButtonEvent(Onclick_CloseDictionary);
     }
 
-    public void Onclick_CloseDictionary()
+    private void OnDisable()
     {
-        UIManager.Instance.CloseUI(UIRootType.ContentUI, UIType.DictionaryUI);
+        if (_slotList.Count > 0)
+        {
+            foreach (var slotKv in _slotList)
+            {
+                var slot = slotKv.Value;
+                DestroyImmediate(slot.gameObject);
+            }
+
+            _slotList.Clear();
+        }
     }
 
+    private void Onclick_CloseDictionary()
+    {
+        UIManager.Instance.CloseContentUI(UIType.DictionaryUI);
+    }
 
+    private void ReadItemListAndCreateSlot()
+    {
+        var dataList = GameDataManager.Instance.ItemDataList;
+        foreach (var dataKv in dataList)
+        {
+            var data = dataKv.Value;
+            if(data == null)
+            {
+                continue;
+            }
+
+            CreateDictionarySlot(data.Id);
+
+            if (_slotList.Count > 0)
+            {
+                foreach (var slotKv in _slotList)
+                {
+                    var slot = slotKv.Value;
+                    slot.Onclick_DictionarySlot();
+                }
+            }
+        }
+    }
+
+    private void CreateDictionarySlot(string dataId)
+    {
+        var gObj = Instantiate(Prefab_Slot, Transform_SlotRoot);
+        if (gObj == null)
+        {
+            return;
+        }
+
+        var slotComponent = gObj.GetComponent<DictionarySlotUI>();
+        if (slotComponent == null)
+        {
+            return;
+        }
+
+        slotComponent.InitSlot(dataId, OnclickChildSlotSelected);
+        _slotList.Add(dataId, slotComponent);
+    }
+
+    private void OnclickChildSlotSelected(string slotDataId)
+    {
+        var currentSelectedData = GameDataManager.Instance.GetItemData(slotDataId);
+        if (currentSelectedData == null) 
+        {
+            return;
+        }
+
+        Text_Name.text = currentSelectedData.Name;
+        Text_Description.text = currentSelectedData.Description;
+        GameUtil.LoadAndSetSpriteImage(Image_MainIcon, currentSelectedData.IconPath).Forget();
+
+        foreach (var slotKv in _slotList)
+        {
+            var slot = slotKv.Value;
+            var dataId = slot.GetSlotDataId();
+            slot.SetSelectedUI(slotDataId == dataId);
+        }
+    }
 }
