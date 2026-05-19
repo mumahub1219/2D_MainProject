9331997c4d7317ca9d742064723a4bc4f6ba5b42
Muma <mumahub1219@gmail.com>
2026-05-19 16:19:27 +0900

    [맵] 타일맵 구성, 카메라 따라가기 기능

---
+++ Assets/Scripts/leeSunHak_Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform Transform_Player;
+    [SerializeField] private float smoothing = 5.0f;
+
+    private float _fixedY;
+    private float _fixedZ;
+
+    private void Start()
+    {
+        _fixedY = transform.position.y;
+        _fixedZ = transform.position.z;
+    }
+
+    private void LateUpdate()
+    {
+        if (Transform_Player == null)
+        {
+            return;
+        }
+
+        Vector3 targetPosition = new Vector3(Transform_Player.position.x, _fixedY, _fixedZ);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+    }
+}
+
