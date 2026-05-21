4d408d2d83c177c7f8d8f950344344dc3d9b485b
Muma <mumahub1219@gmail.com>
2026-05-21 17:16:30 +0900

    [오브젝트] 맵 이동 구현, 다른 물리 적용

---
+++ Assets/Scripts/2D_Object/RockHead.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RockHead : MonoBehaviour
+{
+    [Header("움직임 설정")]
+    [SerializeField] private float _moveDistance = 5.0f;
+    [SerializeField] private float _speed = 2.0f;
+    [SerializeField] private float _fallSharpness = 3.0f;
+
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        RockHeadMovement();
+    }
+
+    private void RockHeadMovement()
+    {
+        float timeFactor = Time.time * _speed;
+        float progress = (Mathf.Sin(timeFactor) + 1.0f) * 0.5f;
+
+        float modifiedProgress = Mathf.Pow(progress, _fallSharpness);
+
+        float newY = _startPosition.y + (modifiedProgress * _moveDistance);
+        transform.position = new Vector3(_startPosition.x, newY, _startPosition.z);
+    }
+}
